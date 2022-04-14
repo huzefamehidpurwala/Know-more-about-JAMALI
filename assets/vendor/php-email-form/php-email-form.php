6c56db7a55e7ab7086c30d65ba6c13a9271ab72a
huzefamehidpurwala <jamaliongithub@gmail.com>
2022-04-14 22:35:22 +0530

    New Updates

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -3,10 +3,11 @@
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'huzefa.works@gmail.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
